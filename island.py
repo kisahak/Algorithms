f062d4350a2b89bc9ee9adc357c66fc838ba1b53
Khalid Isahak <69661161+kisahak@users.noreply.github.com>
2023-07-06 17:32:22 -0400

    island.py

--- island.py
+++ island.py
@@ -1,4 +1,8 @@
 import collections
+#LC 200, how many islands are in a grid
+#an island = a "1" or groups of "1"s that are compeletely surrounded by "0"s or at the ends of the matrix 
+#a priority queue and visit set are used to keep track of current and visited points 
+#BSF algoirthm 
 
 def numIslands( grid) -> int:
     if not grid:
@@ -31,7 +35,18 @@ def numIslands( grid) -> int:
                 islands+=1 
     
     return islands 
-        
+#The grid below is an example of one big island, as all "1"s have connected neighbors
+#grid = [["1","1","1","1","0"],
+#        ["1","1","0","1","0"],
+#        ["1","1","0","0","0"],
+#        ["0","0","0","0","0"]] 
+
+#The grid below is an example of one big island, and two small islands, giving a total of three islands 
+#grid = [["1","1","1","0","0"],
+#        ["1","1","0","1","0"],
+#        ["1","1","0","0","0"],
+#        ["0","0","0","0","1"]] 
+
 
 grid = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
 print(grid)
